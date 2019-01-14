a9c9c7b4f95f55588be6aee4a8613d93a9a6f406
Benjamin Parnell <benjaminparnell.94@gmail.com>
2019-01-14 12:23:40 +0000

    Extract Layout into its own component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import styled from 'styled-components';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Layout from './components/Layout';
 import Loading from './components/Loading';
 const ListsContainer = React.lazy(() => import('./components/ListsContainer'));
 const ItemContainer = React.lazy(() => import('./components/ItemContainer'));
 
-const Wrapper = styled.div`
-  margin: 20px auto;
-  max-width: 960px;
-`;
-
 render(
-  <Wrapper>
+  <Layout>
     <Router>
       <React.Suspense fallback={<Loading />}>
         <Route path="/" exact={true} component={() => <ListsContainer />} />
@@ -25,6 +20,6 @@ render(
         />
       </React.Suspense>
     </Router>
-  </Wrapper>,
+  </Layout>,
   document.getElementById('root')
 );
---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  margin: 20px auto;
+  max-width: 960px;
+`;
+
+export default Layout;
